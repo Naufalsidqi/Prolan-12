656f04d311d4eb2ea60297e2288c36660de0822a
Naufalsidqi <naufalsidqi11@gmail.com>
2017-02-28 21:39:16 +0700

    Add files via upload

---
+++ Minggu 2/mahasiswa.java
@@ -0,0 +1,7 @@
+public class mahasiswa {
+  public static void main(String argv []){
+      System.out.println("Nama	: Naufal Sidqi");
+	  System.out.println("NIM	: 10112619");
+	  System.out.println("Alamat	: Cibiru");
+	  }
+}
